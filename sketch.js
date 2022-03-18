c9cee68665a09a53edccf12b70f57c1a5891a2a3
GerardoAndre <101846886+GerardoAndre@users.noreply.github.com>
2022-03-17 19:02:34 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var box;
+
+function setup() {
+  createCanvas(637,800);
+  box = createSprite(200,200,50,50);
+}
+
+function draw() 
+{
+  background(30);
+
+if(keyIsDown(RIGHT_ARROW)){
+box.x = box.x +5;
+
+}
+
+if(keyIsDown(LEFT_ARROW)){
+  box.x = box.x -5;
+  
+  }
+
+  if(keyIsDown(UP_ARROW)){
+    box.y = box.y -5;
+    
+    }
+
+    if(keyIsDown(DOWN_ARROW)){
+      box.y = box.y +5;
+      
+      }
+
+
+  drawSprites();
+
+}
+
+
+
+
